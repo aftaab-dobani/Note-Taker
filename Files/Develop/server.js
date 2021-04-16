de424e63921c27424d42b4cfca53e56d0d31a918
Aftaab Dobani <1234dobani@gmail.com>
2021-04-16 16:52:21 -0400

    got notes working

--- Files/Develop/server.js
+++ Files/Develop/server.js
@@ -1,40 +1,54 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const uniqid = require('uniqid');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const uniqid = require("uniqid");
+const db = require("./db/db.json");
 
 //DOCUMENTATION
-const app = express(); 
-const PORT = 3000; 
+const app = express();
+const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static("public"));
 //add middleware for public folder
 
-//!!HTML ROUTE 
+//!!HTML ROUTE
 // GET Notes Route ==> return notes.html
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 // GET * ==> return index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
-
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
 
 //!!API ROUTE
 // GET /api/notes ==> read 'db.json and return all saved notes
-app.get('/api/notes', (req, res) => res.json(notes));
-
-
+app.get("/api/notes", (req, res) => {
+  console.log("get notes route");
+  fs.readFileSync("./db/db.json", db);
+  res.json(db);
+});
 
 // 'POST /api/notes ==> receive a new note to save to on the request body and add it to the db.json file. *Look into NPM packages for unique ID for notes*
-app.post('/api/notes', (req, res) => {
+app.post("/api/notes", (req, res) => {
+  let notes = req.body;
+  notes.id = uniqid();
+  console.log(notes);
+  db.push(notes);
+  fs.writeFile("./db/db.json", JSON.stringify(db), "utf-8", (res, err) => {
+    if (err) throw err;
+  });
+  res.json(db);
+});
 
-})
+// 1. req.body
+// 2. fs.readFile
+// 3. JSON.parse
+// 4.
 
-//PORT TO LISTEN 
+//PORT TO LISTEN
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
-  }); 
+  console.log(`App listening on PORT: ${PORT}`);
+});
