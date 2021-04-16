d1f359d7a1c3005210fc44424d9b1abbc5136184
Aftaab Dobani <1234dobani@gmail.com>
2021-04-15 21:44:55 -0400

    working on API route to save notes

--- Files/Develop/server.js
+++ Files/Develop/server.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 //DOCUMENTATION
 const app = express(); 
 const PORT = 8080; 
+const path = require("path");
 
 //!!HTML ROUTE 
 // GET Notes Route ==> return notes.html
@@ -11,17 +12,21 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './assets/notes.html'));
 })
 // GET * ==> return index.html
-app.get('/index', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './assets/index.html'));
 })
 
 
 //!!API ROUTE
 // GET /api/notes ==> read 'db.json and return all saved notes
-app.get('/api/notes', (req, res) => res.json(db)); 
+module.exports = (app) => {
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile("/db.json")
+}
 
 // 'POST /api/notes ==> receive a new note to save to on the request body and add it to the db.json file. *Look into NPM packages for unique ID for notes*
-
+app.post('/api/notes', (req, res) => 
+res.json(db));
 
 
