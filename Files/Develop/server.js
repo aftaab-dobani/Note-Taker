05cf2585a873b8c89087a947f7922797944f037b
Aftaab Dobani <1234dobani@gmail.com>
2021-04-15 23:28:28 -0400

    added listner

--- Files/Develop/server.js
+++ Files/Develop/server.js
@@ -1,32 +1,37 @@
 const express = require('express');
 const fs = require("fs");
+const path = require("path");
 
 //DOCUMENTATION
 const app = express(); 
 const PORT = 8080; 
-const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//add middleware for public folder
 
 //!!HTML ROUTE 
 // GET Notes Route ==> return notes.html
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './assets/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 // GET * ==> return index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './assets/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
 
 //!!API ROUTE
 // GET /api/notes ==> read 'db.json and return all saved notes
-module.exports = (app) => {
+//module.exports = (app) => {
 
-app.get('/api/notes', (req, res) => {
-    fs.readFile("/db.json")
-}
+// app.get('/api/notes', (req, res) =>
 
 // 'POST /api/notes ==> receive a new note to save to on the request body and add it to the db.json file. *Look into NPM packages for unique ID for notes*
-app.post('/api/notes', (req, res) => 
-res.json(db));
+//app.post('/api/notes', (req, res) => 
+//res.json(db));
 
 
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+  }); 
